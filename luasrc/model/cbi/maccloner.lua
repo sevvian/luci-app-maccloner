f80eb4360bc52e45e397e78c0a377340c0786f05
sevvian <59226487+sevvian@users.noreply.github.com>
2026-05-23 23:23:31 -0700

    Create maccloner.lua

---
+++ luasrc/model/cbi/maccloner.lua
@@ -0,0 +1,18 @@
+local m = Map("maccloner", translate("MAC Cloner"),
+    translate("Configure custom MAC addresses for wireless client interfaces."))
+
+local s = m:section(TypedSection, "global", translate("Global Settings"))
+s.addremove = false
+s:option(Flag, "enabled", translate("Enable"), translate("Enable MAC Cloner service"))
+
+local r = m:section(TypedSection, "rule", translate("MAC Cloner Rules"),
+    translate("Define interfaces, target SSIDs, and custom MAC addresses."))
+r.addremove = true
+r.template = "cbi/tblsection"
+
+r:option(Value, "interface", translate("Interface"))
+r:option(Value, "target_ssid", translate("Target SSID (optional)"))
+r:option(Value, "custom_mac", translate("Custom MAC Address"))
+r:option(Flag, "enabled", translate("Enabled"))
+
+return m
